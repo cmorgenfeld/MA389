429d55cc964f412347dd05925d0307f17ad975f7
cmorgenfeld <cmorgenfeld@gmail.com>
2023-04-10 01:49:32 -0400

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,24 @@
+import Chart from 'chart.js/auto';
+
+
+let labels = ['Big Mac', 'Quarter Pounder with Cheese', 'McDouble', 'Double Cheeseburger', 'Hamburger'];
+
+let itemData = [550, 520, 400, 450, 250];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        data: itemData,
+        backgroundColor: 'rgb(66, 221, 245)'
+    }]
+};
+
+const config = {
+    type: 'bar',
+    data: data
+};
+
+const chart = new Chart(
+    document.getElementById('plot'),
+    config
+);
